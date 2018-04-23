6f78f1a639cc0622072d17385ca216cb1b59f8aa
alu20482156n <alu20482156n@192.168.201.102>
2018-04-23 18:35:38 +0200

    first commit

---
+++ Snake/src/DirectionType.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alu20482156n
+ */
+public enum DirectionType {
+
+    NoShape, ZShape, SShape, LineShape, TShape,
+    SquareShape, LShape, MirroredLShape
+
+}
+
---
+++ Snake/src/ScoreBoard.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alu20482156n
+ */
+public class ScoreBoard {
+    
+    private int score;
+}
