83381064a06e0071feff074bdc0f8f131e3405a6
alu20482156n <alu20482156n@192.168.201.102>
2018-05-07 18:55:22 +0200

    records dialog OK_2

--- Snake/src/Game.java
+++ Snake/src/Game.java
@@ -36,6 +36,7 @@ public class Game extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         iniGame = new javax.swing.JMenuItem();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -65,6 +66,14 @@ public class Game extends javax.swing.JFrame {
         });
         jMenu1.add(iniGame);
 
+        jMenuItem1.setText("Show records");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
         jMenuBar1.add(jMenu1);
 
         setJMenuBar(jMenuBar1);
@@ -78,6 +87,10 @@ public class Game extends javax.swing.JFrame {
         
 
     }//GEN-LAST:event_iniGameActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -119,6 +132,7 @@ public class Game extends javax.swing.JFrame {
     private javax.swing.JMenuItem iniGame;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private ScoreBoard scoreBoard;
     // End of variables declaration//GEN-END:variables
 }
